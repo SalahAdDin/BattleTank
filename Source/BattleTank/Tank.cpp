1e9165945c53b2c3d9510abaa47a45f76e69af1b
José Luis <alagunasalahaddin@live.com>
2019-02-07 15:57:29 +0300

    [BattleTank] :sunny: Create Default Sub Objects in C++
    - Added a new TankAiming ActorComponent.
    - Added the new component to the Tank by default.
    - Moved the AimAt logic to the component.

--- Source/BattleTank/Tank.cpp
+++ Source/BattleTank/Tank.cpp
@@ -8,13 +8,14 @@ ATank::ATank()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// No need to protect points as added at construction
+	TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
+
 }
 
 void ATank::AimAt(FVector HitLocation)
 {
-	auto OurTankName = GetName();
-	// UE_LOG(LogTemp, Warning, TEXT("Look direction: %s"), *HitLocation.ToString());
-	UE_LOG(LogTemp, Warning, TEXT("%s aim at: %s"), *OurTankName, *HitLocation.ToString());
+	TankAimingComponent->AimAt(HitLocation);
 }
 
 // Called when the game starts or when spawned
