c0a52511bc9363362bbc91adb68c23c5924824dc
José Luis <alagunasalahaddin@live.com>
2019-02-13 16:58:35 +0300

    [BattleTank] :sunny: Completing Manual Tank Movement
    - Inverted the Aim control input (at up aim will move up now).
    - Added a new input to turn right.
    - Added a new function to turn right.
    Note: TurnRight receive also -1 to +1 values, being negative values turn left.

--- Source/BattleTank/TankMovementComponent.cpp
+++ Source/BattleTank/TankMovementComponent.cpp
@@ -5,7 +5,6 @@
 
 void UTankMovementComponent::Initialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
 {
-	if (!LeftTrackToSet || !RightTrackToSet) { return; }
 	LeftTrack = LeftTrackToSet;
 	RightTrack = RightTrackToSet;
 }
@@ -13,10 +12,16 @@ void UTankMovementComponent::Initialize(UTankTrack* LeftTrackToSet, UTankTrack* 
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
-	//auto Time = GetWorld()->GetTimeSeconds();
-	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
 
+	if (!LeftTrack || !RightTrack) return;
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(Throw);
 	// TODO prevent double-speed due to dual control use
+}
+
+void UTankMovementComponent::IntendTurnRight(float Throw)
+{
+	if (!LeftTrack || !RightTrack) return;
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(-Throw);
 }
